0a883964900c35c4bbb9fb745dfb0e8041de5c0d
V0rsity <163366378+V0rsity@users.noreply.github.com>
2026-01-28 08:59:17 -0500

    one of my meta tags deprecated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
           {/* iOS support */}
           <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         </Head>
 
