678181ff79b64659bef8849b57210eeb138e6940
V0rsity <163366378+V0rsity@users.noreply.github.com>
2026-03-07 13:10:38 -0500

    PWA test 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -114,14 +114,6 @@ export default function Landing() {
 
   useEffect(() => {
     if (loading || user) return;
-    const isPwa =
-      window.matchMedia('(display-mode: standalone)').matches ||
-      (navigator as any).standalone === true;
-    if (isPwa) router.replace('/login');
-  }, [loading, user, router]);
-
-  useEffect(() => {
-    if (loading || user) return;
 
     const observer = new IntersectionObserver(
       (entries) => {
